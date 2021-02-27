f78bf3fb06579d5f8c71bd23f5f5767e9516fc5d
anjuhypp <anjuhypp@gmail.com>
2021-02-27 00:51:56 -0500

    add list items

--- emojiStories.js
+++ emojiStories.js
@@ -1,23 +1,40 @@
+//NOTES
 //Create a button: Grab form, addEventListener, submit, preventDefault
 //Generate emojis: Grab emojis & use Math.random method & user input
 //Generate length (via user input): Grab user input for emoji length
 //Display the emojis: Grab p-tag to display
-const formOne = document.querySelector("#form-one")
-const storyLength = document.querySelector("#story-length-input") //points to user input
-const emojiStory = document.querySelector("#emoji-story")
-const storyButton = document.querySelector("#generate-story-btn")
+
+// const formOne = document.querySelector("#form-one")//Do we need a form?
+const storyLength = document.querySelector("#story-length-input"); //points to user input
+const emojiStory = document.querySelector("#emoji-story");
+const storyButton = document.querySelector("#generate-story-btn");
 
 const generateEmoji = () => {
-    // question why userInput variable needs to be in function
-    const userInput = Number(storyLength.value) //user input(input tag has value property which is a string)
-    for (let i = 0; i < userInput; i++) {
-        const randomIdx = Math.floor(Math.random() * emojis.length-1)
-        emojiStory.innerText += emojis[randomIdx] //Be sure not to reassign (=), make sure to use +=
-    }
-}
+  // question why userInput variable needs to be in function
+  const numInput = Number(storyLength.value); //user input(input tag has value property which is a string)
+  for (let i = 0; i < numInput; i++) {
+    const randomIdx = Math.floor(Math.random() * emojis.length - 1);
+    emojiStory.innerText += emojis[randomIdx]; //Be sure not to reassign (=), make sure to use +=
+  }
+};
 
-storyButton.addEventListener ("click", generateEmoji)
+storyButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  generateEmoji();
+});
 
-formOne.addEventListener ('click', (event) => {
-    event.preventDefault()
-})
+const storyInput = document.querySelector("#description-input");
+const descriptionButton = document.querySelector("#submit-description-btn");
+const ul = document.querySelector("ul")
+const createStory = () => {
+  const storyString = storyInput.value;
+  const li = document.createElement("li");
+  li.textContent = storyString;
+  ul.appendChild(li);
+};
+
+descriptionButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    createStory();
+});
+debugger;
